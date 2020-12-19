ee929de58dfe04155bce285d02a5fc9d08a74845
titanfat <tit22an@gmail.com>
2020-12-19 15:45:50 +0300

    Resource portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :portfolios
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   resources :blogs
   root 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
