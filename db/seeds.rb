11d87efe12c92e6dac851ac1be1498345ab54428
titanfat <tit22an@gmail.com>
2020-12-19 16:11:11 +0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+9.times do |portfolio_items|
+  Portfolio.create!(
+      title: "Portfolio title: #{portfolio_items}",
+      subtitle: "My great service",
+      body: "Lorem ipsum.",
+      main_image:"https://via.placeholder.com/600x200",
+      thumb_image:"https://via.placeholder.com/350x200"
+  )
+      end
