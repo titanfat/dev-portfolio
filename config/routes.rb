bbf430f3d7968f1549d09617f9786ec92afaa197
titanfat <tit22an@gmail.com>
2020-12-20 17:45:42 +0300

    Updating status

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'about-me', to: 'pages#about'
   get 'contact',  to: 'pages#about'
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   root 'pages#home'
