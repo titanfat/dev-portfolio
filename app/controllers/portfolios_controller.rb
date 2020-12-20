9ccea7fd5b521de3c3a0ec22d2daa3d0d649fe25
titanfat <tit22an@gmail.com>
2020-12-20 07:21:29 +0300

    Portfolio update features

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,59 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: %i[show edit update destroy]
+
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def show; end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def edit; end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |f|
+      if @portfolio_item.save
+        f.html { redirect_to @portfolio_item, notice: 'Blog was successfully created.' }
+        f.json { render :show, status: :created, location: @portfolio_item }
+      else
+        f.html { render :new }
+        f.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |f|
+      if @portfolio_item.update(portfolio_params)
+        f.html { redirect_to @portfolio_item, notice: 'Blog was successfully updated.' }
+        f.json { render :show, status: :ok, location: @portfolio_item }
+      else
+        f.html { render :edit }
+        f.json { render json: @portfolio_item.erorrs, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |f|
+      f.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      f.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
   end
 end
