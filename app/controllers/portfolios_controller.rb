0b3ecd4840de9095e6c10b494106c3450e5d476b
titanfat <tit22an@gmail.com>
2020-12-22 09:05:11 +0300

    Implemented Technologies templates

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,5 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio, only: %i[show edit update destroy]
+  before_action :set_portfolio, only: %w[show edit update destroy]
 
   def index
     @portfolio_items = Portfolio.all
@@ -15,6 +15,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
+    3.times{ @portfolio_item.technologies.build }
   end
 
   def create
@@ -58,6 +59,8 @@ class PortfoliosController < ApplicationController
   end
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image)
+    params.require(:portfolio).permit(:title, :subtitle,
+                                      :body, :main_image, :thumb_image,
+                                      technologies_attributes: [:name])
   end
 end
