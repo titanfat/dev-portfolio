cf27b6c3993a2dc0e79b3ca0e04af3d6db16ace0
titanfat <tit22an@gmail.com>
2020-12-22 03:27:42 +0300

    Implemented image default

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,12 @@
 class Portfolio < ApplicationRecord
-  validates_presence_of :title, :body, :subtitle
+  validates_presence_of :title, :body, :main_image, :thumb_image
   scope :ror_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
   scope :angular, -> { where(subtitle: 'Angular') }
+
+  after_initialize :set_default
+
+  def set_default
+    self.main_image ||= 'https://via.placeholder.com/600x200'
+    self.thumb_image ||= 'https://via.placeholder.com/350x200'
+  end
 end
