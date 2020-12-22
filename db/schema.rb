b1db03a0afc18a07a420afc19eeda277d2ee1e36
titanfat <tit22an@gmail.com>
2020-12-22 07:28:49 +0300

    Created Technology model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_004459) do
+ActiveRecord::Schema.define(version: 2020_12_22_035954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 2020_12_22_004459) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "badge"
+  end
+
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolio_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["portfolio_id"], name: "index_technologies_on_portfolio_id"
   end
 
   create_table "topics", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,8 @@ puts '3 Topic created'
   )
 end
 
+puts '9 portfolio created'
+
 5.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -40,12 +42,24 @@ puts '5 blog created'
   )
 end
 
+puts '3 skill created'
+
 1.times do |portfolio_item|
   Portfolio.create!(
-      title: "Portfolio title: #{portfolio_item}",
-      subtitle: "Angular",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      main_image: "https://via.placeholder.com/600x200",
-      thumb_image: "https://via.placeholder.com/350x200"
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "https://via.placeholder.com/600x200",
+    thumb_image: "https://via.placeholder.com/350x200"
   )
 end
+
+puts '1 Angular portfolio created'
+
+3.times do |tech|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{tech}"
+  )
+end
+
+puts '3 Technology created'
