fb620ed04279c487fdb88cfd8a7a6c875a317039
titanfat <tit22an@gmail.com>
2020-12-23 02:52:05 +0300

    Set title and tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_title
   include DeviseWhitelist
   include SetSource
   include CurrentUser
+
+  def set_title
+    @page_title = "Andrew Titov Developer| My Portfolio Website"
+    @seo_keywords = "Andrew Titov portfolio"
+  end
 end
