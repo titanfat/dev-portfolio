13315eb2da76f109540784bfb413daa706918f6a
titanfat <tit22an@gmail.com>
2020-12-24 12:06:49 +0300

    Update style and tags home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable
 
   validates_presence_of :name
 
