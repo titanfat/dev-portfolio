2a1493f08d2709145f720e8d4c3149b3e3b04d56
titanfat <tit22an@gmail.com>
2020-12-26 14:19:03 +0300

    Complete drag and drop func portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -15,6 +15,6 @@ class Portfolio < ApplicationRecord
 
   def set_default
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+    self.thumb_image ||= Placeholder.image_generator(height: '400', width: '200')
   end
 end
